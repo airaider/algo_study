f301caff99dd59793b3bed7568e5f3e75ef5bf21
airaider <airaider@naver.com>
2019-08-08 19:31:07 +0900

    190808

--- baekjun/src/samsung/Main_14500.java
+++ baekjun/src/samsung/Main_14500.java
@@ -1,14 +1,9 @@
 package samsung;
 
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
-
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
 
 public class Main_14500 {
 	static int N,M;
@@ -39,9 +34,52 @@ public class Main_14500 {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
 				dfs(i,j,0,0);
+				check(i,j,0,0);
 			}
 		}
 		System.out.println(ans);
+	}
+	private static void check(int x, int y, int cnt, int sum) {
+		int flag=0;
+		for(int i=0;i<4;i++) {
+			int nx=x+dir[i][0];
+			int ny=y+dir[i][1];
+			if(nx<0 || nx>=N || ny<0 || ny>=M) continue;
+			flag++;
+		}
+		if(flag<3) {
+			return;
+		}
+		else if(flag==3) {
+			sum+=map[x][y];
+			for(int i=0;i<4;i++) {
+				int nx=x+dir[i][0];
+				int ny=y+dir[i][1];
+				if(nx<0 || nx>=N || ny<0 || ny>=M) continue;
+				sum+=map[nx][ny];
+			}
+			ans=Math.max(sum,ans);
+			return;
+		}
+		else if(flag==4) {
+			sum+=map[x][y];
+			sum+=map[x+1][y];
+			sum+=map[x-1][y];
+			sum+=map[x][y+1];
+			sum+=map[x][y-1];
+			for(int i=0;i<4;i++) {
+				int temp = sum;
+				int nx=x+dir[i][0];
+				int ny=y+dir[i][1];
+				temp-=map[nx][ny];
+				ans=Math.max(temp,ans);
+			}
+			return;
+			
+		}
+		
+		
+		
 	}
 	private static void dfs(int x, int y, int cnt, int sum) {
 		if(cnt==4) {
