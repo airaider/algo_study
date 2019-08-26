616427f0ed759ee0548d1fb57308dfc2118bb85f
jleun111 <jleun111@naver.com>
2019-08-26 16:52:59 +0900

    190826

--- baekjun/src/samsung/Main_16235_아기상어.java
+++ baekjun/src/samsung/Main_16235_아기상어.java
@@ -1,10 +1,26 @@
 package samsung;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main_16235_아기상어 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static int N;
+	private static int[][] map;
 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		map = new int [N][N];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
 	}
 
 }
