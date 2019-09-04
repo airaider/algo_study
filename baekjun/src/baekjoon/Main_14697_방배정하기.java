201a0bb4033562adead351d5ac04f0062479c3be
airaider <airaider@naver.com>
2019-09-04 17:58:40 +0900

    190904

---
+++ baekjun/src/baekjoon/Main_14697_방배정하기.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_14697_방배정하기 {
+	static int N;
+	static int A,B,C;
+	static boolean flag=false;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i <= 50; i++) {
+			for (int j = 0; j <= 50; j++) {
+				for (int k = 0; k <= 50; k++) {
+					if(A*i+B*j+C*k==N) flag=true;
+				}
+			}
+		}
+		System.out.println(flag?1:0);
+	}
+	
+
+}
