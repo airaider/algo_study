d4ce2919e6831bfdea4f49f672d79481ca22dc55
airaider <airaider@naver.com>
2019-09-20 16:41:17 +0900

    190920

---
+++ co_test/src/line/강_탐사.java
@@ -0,0 +1,13 @@
+package line;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 강_탐사 {
+	static int N;
+	static int[][] map;
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+}
